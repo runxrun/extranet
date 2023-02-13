58483dfa7946cf54f0e752523597140ff059b635
NIRUN <nirun@11infotech.com>
2023-02-13 14:53:47 +0700

    [update] - create promotion

--- dynamic-rate/includes/dynamic-rate-promotion-lists.php
+++ dynamic-rate/includes/dynamic-rate-promotion-lists.php
@@ -59,6 +59,377 @@
 	</div>
 </div>
 
+<div class="modal fade" id="modalPromotionAddEdit" tabindex="-1" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="fs-6 fw-bold text-dark">Add / Update Promotions</div>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+
+				<div class="row">
+					<div class="pb-3 col-12 col-lg-6">
+						<div class="fs-8 text-secondary text-truncate">
+							Room category
+						</div>
+						<div class="pt-1">
+							<select class="form-select rounded-0 fs-7">
+								<option value="">All room category</option>
+								<option value="">Standard</option>
+								<option value="">Deluxe</option>
+								<option value="">Family Suite</option>
+							</select>
+						</div>
+					</div>
+					<div class="pb-3 col-12 col-lg-6">
+						<div class="fs-8 text-secondary text-truncate">
+							Meal type
+						</div>
+						<div class="pt-1">
+							<select class="form-select rounded-0 fs-7">
+								<option value="">All meal type</option>
+								<option value="">ABF</option>
+								<option value="">Asian Breakfast</option>
+								<option value="">Bed & Breakfast</option>
+								<option value="">Buffet Breakfast</option>
+								<option value="">Room Only</option>
+							</select>
+						</div>
+					</div>
+				</div>
+				<div class="alert alert-warning d-flex py-2" role="alert">
+					<div class="pe-1 fs-7">
+						<i class="fas fa-exclamation-circle"></i>
+					</div>
+					<div class="ps-1 fs-7">
+				 		Choose the promotion that you wish to generate.
+					</div>
+				</div>
+				<div>
+					<div class="row gy-1 gx-4">
+					<?php
+					$promotype = array(
+						array('name'=>'Advance Purchase Rate (by Period)', ),
+						array('name'=>'Advance Purchase Rate (by Notice Day)', ),
+						array('name'=>'Bonus Night (Free Nights Earned)', ),
+						array('name'=>'Long Stay / Pay Less', ),
+					);
+					?>
+					<?php 
+					foreach ($promotype as $key => $value)
+					{
+						$checked = ($key==0 ? 'checked' : '');
+					?>
+						<div class="col-12 col-lg-6">
+							<div class="form-check">
+								<input
+									type="radio"
+									class="form-check-input"
+									name="<?php echo 'rdoPromotion_'.$key_name; ?>"
+									id="<?php echo 'rdoPromo_'.$key; ?>"
+									<?php echo $checked; ?>>
+								<label class="form-check-label fs-7 text-secondary" for="<?php echo 'rdoPromo_'.$key; ?>">
+									<?php echo $value['name']; ?>
+								</label>
+							</div>
+						</div>
+					<?php 
+					} 
+					?>
+					</div>
+				</div>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary fs-8 rounded-0" data-bs-dismiss="modal">
+                	Close
+                </button>
+                <button class="btn btn-primary fs-8 rounded-0 px-4" data-bs-target="#modalPromotionAddEdit2" data-bs-toggle="modal">
+	                Continue
+	            </button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="modalPromotionAddEdit2" tabindex="-1" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
+    <div class="modal-dialog modal-xl modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="fs-6 fw-bold text-dark">Add / Update Promotions</div>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+
+				<div class="alert alert-warning d-flex py-2" role="alert">
+					<div class="pe-1 fs-7">
+						<i class="fas fa-exclamation-circle"></i>
+					</div>
+					<div class="ps-1 fs-7">
+				 		Advance Purchase Rate (by Period) Detail
+					</div>
+				</div>
+            	
+
+				<div class="row gx-3">
+					<div class="pb-3 col-12 col-lg-3">
+						<div class="pb-1 fs-8 text-secondary">
+							Minimum stay
+						</div>
+						<div>
+							<input type="number" class="form-control rounded-0 fs-7 text-center" value="0" min="0">
+						</div>
+					</div>
+					<div class="pb-3 col-12 col-lg-9">
+						<div class="row gx-3">
+							<div class="pb-3 col-12 col-lg-4">
+								<div class="pb-1 fs-8 text-secondary">
+									Book from date
+								</div>
+								<div class="input-group">
+									<input type="text" class="form-control rounded-0 fs-7" value="01 Oct 2022">
+									<span class="input-group-text rounded-0">
+										<i class="fas fa-calendar-day"></i>
+									</span>
+								</div>
+							</div>
+							<div class="pb-3 col-12 col-lg-4">
+								<div class="pb-1 fs-8 text-secondary">
+									Book to date
+								</div>
+								<div class="input-group">
+									<input type="text" class="form-control rounded-0 fs-7" value="30 Sep 2023">
+									<span class="input-group-text rounded-0">
+										<i class="fas fa-calendar-day"></i>
+									</span>
+								</div>
+							</div>
+						</div>
+						<div class="row gx-3">
+							<div class="pb-3 col-12 col-lg-4">
+								<div class="pb-1 fs-8 text-secondary">
+									Rate from date
+								</div>
+								<div class="input-group">
+									<input type="text" class="form-control rounded-0 fs-7" value="01 Jan 2023">
+									<span class="input-group-text rounded-0">
+										<i class="fas fa-calendar-day"></i>
+									</span>
+								</div>
+							</div>
+							<div class="pb-3 col-12 col-lg-4">
+								<div class="pb-1 fs-8 text-secondary">
+									Rate to date
+								</div>
+								<div class="input-group">
+									<input type="text" class="form-control rounded-0 fs-7" value="30 Sep 2023">
+									<span class="input-group-text rounded-0">
+										<i class="fas fa-calendar-day"></i>
+									</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+
+				<div class="border-top py-3">
+					<div class="row g-3">
+						<div class="col-12 col-lg-3">
+							<div class="d-flex">
+								<span class="fs-7 fw-bold text-dark pe-2">
+									<i class="far fa-window-maximize"></i>
+								</span>
+								<span class="fs-7 fw-bold text-dark">
+									Applicable for
+								</span>
+							</div>
+						</div>
+						<div class="col-12 col-lg-9">
+							<div class="row g-3">
+								<div class="col-12 col-lg-4">
+									<div class="row">
+										<div class="col-12">
+											<div class="pb-2 form-check fs-7 text-secondary">
+								                <input class="form-check-input rounded-0" type="checkbox" id="selected_all" checked="">
+								                <label class="form-check-label fw-bold text-dark" for="selected_all">Select all</label>
+								            </div>
+										</div>
+										<div class="col-12">
+								            <div class="form-check fs-7 text-secondary">
+								                <input class="form-check-input rounded-0" type="checkbox" id="dayflag_mon" checked="">
+								                <label class="form-check-label" for="dayflag_mon">Monday</label>
+								            </div>
+										</div>
+										<div class="col-12">
+								            <div class="form-check fs-7 text-secondary">
+								                <input class="form-check-input rounded-0" type="checkbox" id="dayflag_tue" checked="">
+								                <label class="form-check-label" for="dayflag_tue">Tueday</label>
+								            </div>
+										</div>
+										<div class="col-12">
+								            <div class="form-check fs-7 text-secondary">
+								                <input class="form-check-input rounded-0" type="checkbox" id="dayflag_wed" checked="">
+								                <label class="form-check-label" for="dayflag_wed">Wednesday</label>
+								            </div>
+										</div>
+										<div class="col-12">
+								            <div class="form-check fs-7 text-secondary">
+								                <input class="form-check-input rounded-0" type="checkbox" id="dayflag_thu" checked="">
+								                <label class="form-check-label" for="dayflag_thu">Thursday</label>
+								            </div>
+										</div>
+										<div class="col-12">
+								            <div class="form-check fs-7 text-secondary">
+								                <input class="form-check-input rounded-0" type="checkbox" id="dayflag_fri" checked="">
+								                <label class="form-check-label" for="dayflag_fri">Friday</label>
+								            </div>
+										</div>
+										<div class="col-12">
+								            <div class="form-check fs-7 text-secondary">
+								                <input class="form-check-input rounded-0" type="checkbox" id="dayflag_sat" checked="">
+								                <label class="form-check-label" for="dayflag_sat">Saturday</label>
+								            </div>
+										</div>
+										<div class="col-12">
+								            <div class="form-check fs-7 text-secondary">
+								                <input class="form-check-input rounded-0" type="checkbox" id="dayflag_sun" checked="">
+								                <label class="form-check-label" for="dayflag_sun">Sunday</label>
+								            </div>
+										</div>
+									</div>
+								</div>
+								<div class="col-12 col-lg-4">
+									<div class="pb-2 form-check fs-7 text-secondary">
+								        <input class="form-check-input rounded-0" type="checkbox" id="selected_all_roomtype" checked="">
+								        <label class="form-check-label fw-bold text-dark" for="selected_all_roomtype">Select all</label>
+								    </div>
+								    <div class="form-check fs-7 text-secondary">
+								        <input class="form-check-input rounded-0" type="checkbox" id="sgtwdb" checked="">
+								        <label class="form-check-label" for="sgtwdb">Single / Twin / Double</label>
+								    </div>
+								    <div class="form-check fs-7 text-secondary">
+								        <input class="form-check-input rounded-0" type="checkbox" id="quad" checked="">
+								        <label class="form-check-label" for="quad">Quad</label>
+								    </div>
+								    <div class="form-check fs-7 text-secondary">
+								        <input class="form-check-input rounded-0" type="checkbox" id="extrabed" checked="">
+								        <label class="form-check-label" for="extrabed">Extra bed</label>
+								    </div>
+								    <div class="form-check fs-7 text-secondary">
+								        <input class="form-check-input rounded-0" type="checkbox" id="extrabedchild" checked="">
+								        <label class="form-check-label" for="extrabedchild">Extra bed - child</label>
+								    </div>
+								</div>
+								<div class="col-12 col-lg-4">
+									<div class="pb-2 form-check fs-7 text-secondary">
+								        <input class="form-check-input rounded-0" type="checkbox" id="selected_all_meal" checked disabled>
+								        <label class="form-check-label fw-bold text-dark" for="selected_all_meal">Select all</label>
+								    </div>
+								    <div class="form-check fs-7 text-secondary">
+								        <input class="form-check-input rounded-0" type="checkbox" id="bfad" checked disabled>
+								        <label class="form-check-label" for="bfad">Breakfast adult</label>
+								    </div>
+								    <div class="form-check fs-7 text-secondary">
+								        <input class="form-check-input rounded-0" type="checkbox" id="bfch" checked disabled>
+								        <label class="form-check-label" for="bfch">Breakfast child</label>
+								    </div>
+								</div>
+							</div>
+							<div class="mt-3 row g-3">
+						        <div class="col-12 col-lg-4">
+									<div class="pb-1 fs-8 text-secondary">
+										Promotion code
+									</div>
+									<div>
+										<input type="text" class="form-control rounded-0 fs-7 text-center" value="">
+									</div>
+						        </div>
+						        <div class="col-12 col-lg-4">
+									<div class="pb-1 fs-8 text-secondary">
+										Discount
+									</div>
+									<div>
+										<select class="form-select rounded-0 fs-7">
+											<option>Please select</option>
+											<option>Amount</option>
+											<option selected="">Percent</option>
+										</select>
+									</div>
+						        </div>
+						        <div class="col-12 col-lg-4">
+									<div class="pb-1 fs-8 text-secondary">
+										Discount Rate
+									</div>
+									<div>
+										<input type="number" class="form-control rounded-0 fs-7 text-center" value="10">
+									</div>
+						        </div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+            </div>
+            <div class="modal-footer">
+                <button
+                	type="button"
+                	class="btn btn-secondary fs-8 rounded-0"
+                	data-bs-target="#modalPromotionAddEdit"
+                	data-bs-toggle="modal">
+					Back                		
+                </button>
+                <button
+                	type="button"
+                	class="btn btn-primary fs-8 rounded-0 px-4"
+                	data-bs-dismiss="modal">
+            		Save changes
+            	</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- 
 <div class="border-bottom d-none">
 	<div class="row g-0">
 		<div class="col-3">
@@ -225,68 +596,4 @@
 	</div>
 </div>
 
-
-
-<div class="modal fade" id="modalPromotionAddEdit" tabindex="-1" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
-    <div class="modal-dialog modal-xl modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <div class="fs-6 fw-bold text-dark">Add / Update Promotions</div>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                Show a second modal and hide this one with the button below.
-
-            		<br>
-            		<br>
-            		<br>
-            		<br>
-            		<br>
-            		<br>
-            		<br>
-            		<br>
-
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary fs-8 rounded-0" data-bs-dismiss="modal">Close</button>
-                <button class="btn btn-primary fs-8 rounded-0 px-4" data-bs-target="#modalPromotionAddEdit2" data-bs-toggle="modal">Open second modal</button>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="modal fade" id="modalPromotionAddEdit2" tabindex="-1" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
-    <div class="modal-dialog modal-xl modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <div class="fs-6 fw-bold text-dark">Add / Update Promotions</div>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-
-            	Set promotion here!!
-            		<br>
-            		<br>
-            		<br>
-            		<br>
-            		<br>
-            		<br>
-            		<br>
-            		<br>
-
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary fs-8 rounded-0" data-bs-target="#modalPromotionAddEdit" data-bs-toggle="modal">Back to first</button>
-                <button type="button" class="btn btn-primary fs-8 rounded-0 px-4"  data-bs-dismiss="modal">Save changes</button>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
-
-
-
-
-
-
+ -->
